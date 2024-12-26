96e7d580ff83a9d758528939d66fae86f21787db
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-12-26 12:59:37 -0300

    Como usar variáveis
    Utilização de variáveis para a obtenção de resultados dinâmicos em Querys

---
+++ projeto/estudos/A12variaveis/server.ts
@@ -0,0 +1,86 @@
+import { gql, ApolloServer } from "apollo-server";
+
+//interfaces
+interface Usuario {
+    id: String;
+    nome: String;
+    perfil: String;
+};
+
+interface Perfil {
+    id: String;
+    nome: String;
+};
+
+//bases de dados
+const usuarios: Usuario[] = [
+    {
+        id: "1",
+        nome: "Lucas",
+        perfil: "1"
+    },
+
+    {
+        id: "2",
+        nome: "Joana",
+        perfil: "2"
+    }
+];
+
+enum tipoPerfis {
+    ADMINISTRADOR = "Administrador",
+    USUARIO = "Usuário"
+};
+
+const perfis: Perfil[] = [
+    {
+        id: "1",
+        nome: tipoPerfis.ADMINISTRADOR
+    },
+
+    {
+        id: "2",
+        nome: tipoPerfis.USUARIO
+    }
+];
+
+const typeDefs: any = gql`
+    type Usuario {
+        id: ID!,
+        nome: String!,
+        perfil: Perfil
+    },
+
+    type Perfil {
+        id: ID!,
+        nome: String!
+    },
+
+    type Query {
+        usuario(id: ID!): Usuario      
+    }
+`;
+
+//resoluções dos tipos de objetos
+const resolvers: any = {
+    Usuario: {
+        perfil(usuario: Usuario): Perfil | null {
+            return perfis.find(perfil => perfil.id === usuario.perfil) || null;
+        }
+    },
+    
+    Query: {
+        usuario(_: any, args: {id: String}): Usuario | null {
+            return usuarios.find(usuario => usuario.id === args.id) || null;
+        }
+    }
+};
+
+const server: ApolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(({url}) => {
+    console.log("Servidor rodando com sucesso em: " + url);
+});
