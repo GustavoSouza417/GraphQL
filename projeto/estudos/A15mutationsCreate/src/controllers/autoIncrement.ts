322b2409780a22d431f454c94b7b8d18e72c565f
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-12-28 18:15:18 -0300

    Feature de auto incremento finalizada (faltam as tratativas)

--- projeto/estudos/A15mutationsCreate/src/controllers/autoIncrement.ts
+++ projeto/estudos/A15mutationsCreate/src/controllers/autoIncrement.ts
@@ -2,13 +2,23 @@ import { readFileSync } from "fs";
 import Models from "../tipos/interfaces/json/iModels";
 
 export default function autoIncrement(): String {
-    let models: string;
     let json: Models;
+    let models: string;
+    let key: string;
     let id: number;
+    let increment: number;
 
     models = readFileSync("./src/models/models.json", "utf8");
     json = JSON.parse(models);
 
-    id = (Object.keys(json.usuarios).length) + 1;
-    return id.toString();
+    //busca o valor do ID do último usuário adicionado e adiciona um
+    id = Object.keys(json.usuarios).length - 1;
+    key = Object.keys(json.usuarios)[id];
+    increment = parseInt(json.usuarios[key].id.valueOf()) + 1;
+    
+    return increment.toString();
 };
+
+//as operações de JSON estão com o endereço a partir da pasta raiz
+//porque é o servidor que está executando elas, já que a função
+//está sendo chamada nos resolvers
