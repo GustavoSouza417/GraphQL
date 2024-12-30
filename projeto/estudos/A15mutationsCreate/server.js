35204f943a15ece6c4ad256cb9ae73e5aeefd687
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-12-29 21:41:44 -0300

    Formatação de erros temporária inserida

--- projeto/estudos/A15mutationsCreate/server.js
+++ projeto/estudos/A15mutationsCreate/server.js
@@ -5,7 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const apollo_server_1 = require("apollo-server");
 const merge_1 = __importDefault(require("./src/graphql/merge"));
-const server = new apollo_server_1.ApolloServer({ schema: merge_1.default });
+const server = new apollo_server_1.ApolloServer({
+    schema: merge_1.default,
+    formatError: (err) => {
+        if (err.message.startsWith("O endereço de e-mail fornecido já está cadastrado!"))
+            return new Error(err.message);
+        return err;
+    }
+});
 server.listen().then(({ url }) => {
     console.log("Servidor rodando com sucesso em: " + url);
 });
--- projeto/estudos/A15mutationsCreate/server.ts
+++ projeto/estudos/A15mutationsCreate/server.ts
@@ -1,7 +1,15 @@
 import { ApolloServer } from "apollo-server";
 import schema from "./src/graphql/merge";
 
-const server: ApolloServer = new ApolloServer({ schema });
+const server: ApolloServer = new ApolloServer({ 
+    schema,
+
+    formatError: (err: any) => {
+        if(err.message.startsWith("O endereço de e-mail fornecido já está cadastrado!"))
+            return new Error(err.message);
+        return err;
+    }
+});
 
 server.listen().then(({ url }) => {
     console.log("Servidor rodando com sucesso em: " + url);
